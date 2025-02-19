b0805709a6aec97fe23f3138e3c6ba0738c6f0b1
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-19 18:30:28 -0500

    feat: :sparkles: add validation rules and fields to CreateModuloDto and UpdateModuloDto

--- src/modulos/dto/create-modulo.dto.ts
+++ src/modulos/dto/create-modulo.dto.ts
@@ -1,1 +1,7 @@
-export class CreateModuloDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateModuloDto {
+  @IsNotEmpty({ message: 'El nombre es requerido' })
+  @IsString({ message: 'El nombre debe ser un texto' })
+  nombre: string;
+}
