160a870d40bc4c33daf40bd6800965daca7313bc
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-19 18:24:10 -0500

    feat: :sparkles: add validation rules and fields to CreateContadorPersonaDto and UpdateContadorPersonaDto

--- src/contador_persona/dto/create-contador_persona.dto.ts
+++ src/contador_persona/dto/create-contador_persona.dto.ts
@@ -1,1 +1,15 @@
-export class CreateContadorPersonaDto {}
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateContadorPersonaDto {
+  @IsNotEmpty({ message: 'El id de la persona no puede estar vacío' })
+  @IsInt({ message: 'El id de la persona debe ser un número entero' })
+  id_persona: number;
+
+  @IsNotEmpty({ message: 'El id del contador no puede estar vacío' })
+  @IsInt({ message: 'El id del contador debe ser un número entero' })
+  id_contador: number;
+
+  @IsNotEmpty({ message: 'La targeta profesional no puede estar vacía' })
+  @IsString({ message: 'La targeta profesional debe de ser un string' })
+  targeta_profesional: string;
+}
--- src/contador_persona/dto/update-contador_persona.dto.ts
+++ src/contador_persona/dto/update-contador_persona.dto.ts
@@ -1,6 +1,33 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateContadorPersonaDto } from './create-contador_persona.dto';
+import {
+  IsDate,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
-export class UpdateContadorPersonaDto extends PartialType(CreateContadorPersonaDto) {
+export class UpdateContadorPersonaDto extends PartialType(
+  CreateContadorPersonaDto,
+) {
+  @IsNotEmpty({ message: 'El id de la persona no puede estar vacío' })
+  @IsInt({ message: 'El id de la persona debe ser un número entero' })
   id: number;
+
+  @IsOptional()
+  @IsString({ message: 'La targeta profesional debe de ser un string' })
+  targeta_profesional: string;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de expedicion debe ser una fecha' })
+  created_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de actualizacion debe ser una fecha' })
+  updated_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de eliminacion debe ser una fecha' })
+  deleted_at: Date;
 }
