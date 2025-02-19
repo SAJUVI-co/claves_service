a4a0de5cd8843be6461d78749d6bffbef9281679
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-19 18:17:50 -0500

    feat: :sparkles: enhance CreatePersonaDto and UpdatePersonaDto with validation rules and additional fields

--- src/persona/dto/create-persona.dto.ts
+++ src/persona/dto/create-persona.dto.ts
@@ -1,1 +1,24 @@
-export class CreatePersonaDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export enum TipoPersona {
+  juridica = 'JURIDICA',
+  natural = 'NATURAL',
+}
+
+export class CreatePersonaDto {
+  @IsNotEmpty({ message: 'El tipo de persona no puede estar vacio' })
+  @IsString({ message: 'El tipo de persona debe ser una cadena de texto' })
+  tipo_persona: TipoPersona;
+
+  @IsNotEmpty({ message: 'El nombre de usuario no puede estar vacío' })
+  @IsString({ message: 'El nombre de usuario debe ser una cadena de texto' })
+  nombre: string;
+
+  @IsNotEmpty({ message: 'La cedula no puede estar vacia' })
+  @IsNumber({}, { message: 'La cedula debe ser un número' })
+  cedula: number;
+
+  @IsNotEmpty({ message: 'El dv no puede estar vacio' })
+  @IsInt({ message: 'El correo debe ser Int' })
+  dv: number;
+}
