0ec53878243602d060d5e9d41a6115c966a85f71
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-19 18:30:21 -0500

    fix: :zap: fix updated-persona.dto.ts file
    has been updated the file
    
    160a87

--- src/persona/dto/update-persona.dto.ts
+++ src/persona/dto/update-persona.dto.ts
@@ -1,35 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreatePersonaDto, TipoPersona } from './create-persona.dto';
-import {
-  IsDate,
-  IsInt,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { CreatePersonaDto } from './create-persona.dto';
+import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
 
 // Usar este dto para realizar busquedas
 export class UpdatePersonaDto extends PartialType(CreatePersonaDto) {
   @IsNotEmpty({ message: 'El id no puede estar vacio' })
   @IsInt({ message: 'El id debe ser un número entero' })
   id: number;
-
-  @IsOptional({ message: 'El tipo de persona no puede estar vacio' })
-  @IsString({ message: 'El tipo de persona debe ser una cadena de texto' })
-  tipo_persona: TipoPersona;
-
-  @IsOptional({ message: 'El nombre de usuario no puede estar vacío' })
-  @IsString({ message: 'El nombre de usuario debe ser una cadena de texto' })
-  nombre: string;
-
-  @IsOptional({ message: 'La cedula no puede estar vacia' })
-  @IsNumber({}, { message: 'La cedula debe ser un número' })
-  cedula: number;
-
-  @IsOptional({ message: 'El dv no puede estar vacio' })
-  @IsInt({ message: 'El correo debe ser Int' })
-  dv: number;
 
   @IsOptional()
   @IsDate()
