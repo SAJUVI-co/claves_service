3b179aeb811f0afe42e5e9f76e698f5c044469e6
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-19 18:39:20 -0500

    feat: :sparkles: add validation rules and fields to CreateProgramaDto and UpdateProgramaDto

--- src/programas/dto/create-programa.dto.ts
+++ src/programas/dto/create-programa.dto.ts
@@ -1,1 +1,11 @@
-export class CreateProgramaDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateProgramaDto {
+  @IsNotEmpty({ message: 'El nombre del programa es requerido' })
+  @IsString({ message: 'El nombre del programa debe ser un texto' })
+  nombre: string;
+
+  @IsNotEmpty({ message: 'La pagina web del programa es requerida' })
+  @IsString({ message: 'La pagina web del programa debe ser un texto' })
+  paginaWeb: string;
+}
--- src/programas/dto/update-programa.dto.ts
+++ src/programas/dto/update-programa.dto.ts
@@ -1,6 +1,39 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProgramaDto } from './create-programa.dto';
+import {
+  IsDate,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class UpdateProgramaDto extends PartialType(CreateProgramaDto) {
+  @IsNotEmpty()
+  @IsInt({ message: 'El id del programa debe ser un número entero' })
   id: number;
+
+  @IsOptional()
+  @IsString({ message: 'El nombre del programa debe ser un texto' })
+  nombre: string;
+
+  @IsOptional()
+  @IsString({ message: 'La pagina web del programa debe ser un texto' })
+  paginaWeb: string;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de creación del programa debe ser una fecha' })
+  created_at: Date;
+
+  @IsOptional()
+  @IsDate({
+    message: 'La fecha de actualizacion del programa debe ser una fecha',
+  })
+  udpated_at: Date;
+
+  @IsOptional()
+  @IsDate({
+    message: 'La fecha de eliminacion del programa debe ser una fecha',
+  })
+  deleted_at: Date;
 }
