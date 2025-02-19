009ec22f175e02ace81dfe30629cbbe9f61923de
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-19 18:56:18 -0500

    feat: :sparkles: add validation rules and fields to CreateDianDto and UpdateDianDto

--- src/dian/dto/create-dian.dto.ts
+++ src/dian/dto/create-dian.dto.ts
@@ -1,1 +1,11 @@
-export class CreateDianDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateDianDto {
+  @IsNotEmpty({ message: 'La calve de ingreso es requerida' })
+  @IsString({ message: 'La clave de ingreso debe ser un texto' })
+  clave_ingreso: string;
+
+  @IsOptional()
+  @IsString({ message: 'La clave firma debe ser de tipo string' })
+  clave_firma: string;
+}
--- src/dian/dto/update-dian.dto.ts
+++ src/dian/dto/update-dian.dto.ts
@@ -1,6 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDianDto } from './create-dian.dto';
+import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateDianDto extends PartialType(CreateDianDto) {
+  @IsNotEmpty({ message: 'El id de la dian es requerido' })
+  @IsInt({ message: 'El id de la dian debe ser un número entero' })
   id: number;
+
+  @IsOptional()
+  @IsDate({ message: 'La fechad de creacion debe ser de tipo date' })
+  craeted_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo date' })
+  updated_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo date' })
+  delted_at: Date;
 }
