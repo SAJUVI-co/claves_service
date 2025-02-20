9689a83317a8d6541eadf1a416246b11418b6ccb
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-19 19:21:46 -0500

    feat: :sparkles: add validation rules and fields to CreateCcfDto and UpdateCcfDto

--- src/ccf/dto/create-ccf.dto.ts
+++ src/ccf/dto/create-ccf.dto.ts
@@ -1,1 +1,15 @@
-export class CreateCcfDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCcfDto {
+  @IsNotEmpty({ message: 'El correo electronico es requerido' })
+  @IsString({ message: 'El correo debe ser de tipo texto' })
+  email: string;
+
+  @IsNotEmpty({ message: 'La clave de acceso es requerida' })
+  @IsString({ message: 'La clave de acceso debe ser un texto' })
+  clave: string;
+
+  @IsNotEmpty({ message: 'El nombre codigo de empresa es requerido' })
+  @IsString({ message: 'El nombre codigo de empresa debe ser un texto' })
+  codigo_empresa: string;
+}
--- src/ccf/dto/update-ccf.dto.ts
+++ src/ccf/dto/update-ccf.dto.ts
@@ -1,6 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCcfDto } from './create-ccf.dto';
+import { IsDate, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateCcfDto extends PartialType(CreateCcfDto) {
+  @IsNotEmpty({ message: 'El id de la CCF es requerido' })
+  @IsString({ message: 'El id de la CCF debe ser un número entero' })
   id: number;
+
+  @IsOptional({ message: 'La fecha de creación debe ser una fecha' })
+  @IsDate()
+  created_at: Date;
+
+  @IsOptional({ message: 'La fecha de actualización debe ser una fecha' })
+  @IsDate()
+  updated_at: Date;
+
+  @IsOptional({ message: 'La fecha de eliminación debe ser una fecha' })
+  @IsDate()
+  deleted_at: Date;
 }
