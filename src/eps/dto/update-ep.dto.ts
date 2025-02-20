7adc47d95e6c7c88936e1a81272936450cc1ef91
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 16:40:49 -0500

    fix: :card_file_box: remove unnecessary date fields from UpdateEpDto

--- src/eps/dto/update-ep.dto.ts
+++ src/eps/dto/update-ep.dto.ts
@@ -1,21 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEpDto } from './create-ep.dto';
-import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class UpdateEpDto extends PartialType(CreateEpDto) {
   @IsNotEmpty({ message: 'El campo id no puede estar vacío' })
   @IsInt({ message: 'El campo id debe ser un número entero' })
   id: number;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de creacion debe ser de tipo date' })
-  created_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo date' })
-  updated_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo date' })
-  deleted_at: Date;
 }
