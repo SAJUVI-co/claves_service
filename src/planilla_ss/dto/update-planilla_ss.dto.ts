10a38c38c7cbc267e61ccd29d2974efc777fd613
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 16:41:37 -0500

    fix: :card_file_box: remove unnecessary date fields from UpdatePlanillaSsDto

--- src/planilla_ss/dto/update-planilla_ss.dto.ts
+++ src/planilla_ss/dto/update-planilla_ss.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePlanillaSsDto } from './create-planilla_ss.dto';
-import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class UpdatePlanillaSsDto extends PartialType(CreatePlanillaSsDto) {
   @IsNotEmpty({
@@ -11,16 +11,4 @@ export class UpdatePlanillaSsDto extends PartialType(CreatePlanillaSsDto) {
       'El id de la planilla de seguridad social debe ser un numero entero',
   })
   id: number;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de creacion debe ser una fecha valida' })
-  created_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de actualizacion debe ser una fecha valida' })
-  udpated_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de eliminacion debe ser una fecha valida' })
-  deleted_at: Date;
 }
