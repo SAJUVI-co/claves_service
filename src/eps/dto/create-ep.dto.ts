2eddf6adfb3e2237db1a35496f78ae3365e20cb8
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-19 19:08:27 -0500

    feat: :sparkles: add validation rules and fields to CreateEpDto and UpdateEpDto

--- src/eps/dto/create-ep.dto.ts
+++ src/eps/dto/create-ep.dto.ts
@@ -1,1 +1,11 @@
-export class CreateEpDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateEpDto {
+  @IsNotEmpty({ message: 'El campo usuario no puede estar vacío' })
+  @IsString({ message: 'El campo usuario debe ser un string' })
+  usuario: string;
+
+  @IsNotEmpty({ message: 'la clave de ingreso no puede estar vacia' })
+  @IsString({ message: 'la clave de ingreso debe ser un string' })
+  clave: string;
+}
