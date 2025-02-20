097d3b8e3b068690bc29126030b0548e3cbfd67c
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 16:59:21 -0500

    feat: :sparkles: add properties to Alcaldia entity including id, cuidad, pagina_web, clave_ingreso, clave_exogena, clave_ica, and placa

--- src/alcaldia/entities/alcaldia.entity.ts
+++ src/alcaldia/entities/alcaldia.entity.ts
@@ -1,1 +1,41 @@
-export class Alcaldia {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Alcaldia {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    comment: 'El id de la cuidad',
+  })
+  cuidad: number;
+
+  @Column({
+    nullable: false,
+    comment: 'Pagina web',
+  })
+  pagina_web: string;
+
+  @Column({
+    nullable: false,
+    comment: 'La clave de ingreso es requerida',
+  })
+  clave_ingreso: string;
+
+  @Column({
+    comment: 'Clave de exogenna',
+  })
+  clave_exogena: string;
+
+  @Column({
+    nullable: false,
+    comment: 'Clave de ica',
+  })
+  clave_ica: string;
+
+  @Column({
+    nullable: false,
+    comment: 'Placa',
+  })
+  placa: number;
+}
