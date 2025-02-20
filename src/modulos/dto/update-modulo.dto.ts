d4f7153490a1c871530cc1596ce494f231cd25a8
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 16:40:57 -0500

    fix: :card_file_box: remove unnecessary date fields from UpdateModuloDto

--- src/modulos/dto/update-modulo.dto.ts
+++ src/modulos/dto/update-modulo.dto.ts
@@ -1,21 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateModuloDto } from './create-modulo.dto';
-import { IsDate, IsInt, IsNotEmpty } from 'class-validator';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class UpdateModuloDto extends PartialType(CreateModuloDto) {
   @IsNotEmpty({ message: 'El id es requerido' })
   @IsInt({ message: 'El id debe ser un número entero' })
   id: number;
-
-  @IsNotEmpty({ message: 'La fecha de creacion es requerida' })
-  @IsDate({ message: 'La fecha de creacion debe ser de tipo fecha' })
-  created_at: Date;
-
-  @IsNotEmpty({ message: 'La fecha de actualizacion es requerida' })
-  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo fecha' })
-  udpated_at: Date;
-
-  @IsNotEmpty({ message: 'La fecha de eliminacion es requerida' })
-  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo fecha' })
-  deleted_at: Date;
 }
