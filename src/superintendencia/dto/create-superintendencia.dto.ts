88ba1dd5e6c0e17055760fc63278a83211c17380
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-19 19:12:47 -0500

    feat: :sparkles: add validation rules and fields to CreateSuperintendenciaDto and UpdateSuperintendenciaDto

--- src/superintendencia/dto/create-superintendencia.dto.ts
+++ src/superintendencia/dto/create-superintendencia.dto.ts
@@ -1,1 +1,11 @@
-export class CreateSuperintendenciaDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateSuperintendenciaDto {
+  @IsNotEmpty({ message: 'El usuario es requerido' })
+  @IsString({ message: 'El usuario debe ser un string' })
+  usuario: string;
+
+  @IsNotEmpty({ message: 'La clave de ingreso es requerida' })
+  @IsString({ message: 'La clave de ingreso debe ser un string' })
+  clave: string;
+}
--- src/superintendencia/dto/update-superintendencia.dto.ts
+++ src/superintendencia/dto/update-superintendencia.dto.ts
@@ -1,6 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSuperintendenciaDto } from './create-superintendencia.dto';
+import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
 
-export class UpdateSuperintendenciaDto extends PartialType(CreateSuperintendenciaDto) {
+export class UpdateSuperintendenciaDto extends PartialType(
+  CreateSuperintendenciaDto,
+) {
+  @IsNotEmpty({ message: 'El id es requerido' })
+  @IsInt({ message: 'El id debe ser un número entero' })
   id: number;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de creacion debe ser de tipo date' })
+  craeted_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo date' })
+  updated_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo date' })
+  deleted_at: Date;
 }
