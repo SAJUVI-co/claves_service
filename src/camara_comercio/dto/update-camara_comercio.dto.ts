75189ff80500266e5a0e50656cf09f370dbd54f7
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 16:40:27 -0500

    fix: :card_file_box: remove unnecessary date fields from UpdateCamaraComercioDto

--- src/camara_comercio/dto/update-camara_comercio.dto.ts
+++ src/camara_comercio/dto/update-camara_comercio.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCamaraComercioDto } from './create-camara_comercio.dto';
-import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class UpdateCamaraComercioDto extends PartialType(
   CreateCamaraComercioDto,
@@ -8,16 +8,4 @@ export class UpdateCamaraComercioDto extends PartialType(
   @IsNotEmpty({ message: 'El id es requerido' })
   @IsInt({ message: 'El id debe ser de tipo entero' })
   id: number;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de creacion debe ser de tipo date' })
-  created_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo date' })
-  updated_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo date' })
-  delted_at: Date;
 }
