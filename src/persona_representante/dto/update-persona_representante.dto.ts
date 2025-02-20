9a39ab96c6b86c4997de57e9277c12647bb5edb6
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 16:41:30 -0500

    fix: :card_file_box: remove unnecessary date fields from UpdatePersonaRepresentanteDto

--- src/persona_representante/dto/update-persona_representante.dto.ts
+++ src/persona_representante/dto/update-persona_representante.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePersonaRepresentanteDto } from './create-persona_representante.dto';
-import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class UpdatePersonaRepresentanteDto extends PartialType(
   CreatePersonaRepresentanteDto,
@@ -8,16 +8,4 @@ export class UpdatePersonaRepresentanteDto extends PartialType(
   @IsNotEmpty({ message: 'El id es requerido' })
   @IsInt({ message: 'El id debe ser de tipo entero' })
   id: number;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de creacion debe ser de tipo date' })
-  created_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo date' })
-  updated_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo date' })
-  delted_at: Date;
 }
