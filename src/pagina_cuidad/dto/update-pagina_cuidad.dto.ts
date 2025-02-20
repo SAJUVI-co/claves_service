dd5283a6d935454d0453b245c08c1043b567d53d
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 16:41:14 -0500

    fix: :card_file_box: remove unnecessary date fields from UpdatePaginaCuidadDto

--- src/pagina_cuidad/dto/update-pagina_cuidad.dto.ts
+++ src/pagina_cuidad/dto/update-pagina_cuidad.dto.ts
@@ -1,21 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePaginaCuidadDto } from './create-pagina_cuidad.dto';
-import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class UpdatePaginaCuidadDto extends PartialType(CreatePaginaCuidadDto) {
   @IsNotEmpty({ message: 'El id de la cuidad es requerido' })
   @IsInt({ message: 'El id de la cuidad debe ser un número entero' })
   id: number;
-
-  @IsOptional()
-  @IsDate({ message: 'La fechad de creacion debe ser de tipo date' })
-  craeted_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo date' })
-  updated_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo date' })
-  delted_at: Date;
 }
