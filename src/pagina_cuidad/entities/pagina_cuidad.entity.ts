91756d2f2fbdfcf886926c24da162d7f2c0341bc
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 17:09:33 -0500

    feat: :sparkles: add PaginaCuidad entity with id, cuidad, and timestamp fields

--- src/pagina_cuidad/entities/pagina_cuidad.entity.ts
+++ src/pagina_cuidad/entities/pagina_cuidad.entity.ts
@@ -1,1 +1,26 @@
-export class PaginaCuidad {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class PaginaCuidad {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    comment: 'Nombre de la cuidad',
+  })
+  cuidad: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
