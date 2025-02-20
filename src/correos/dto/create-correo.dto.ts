f326d68912006f3d4964ed68490600947e409da2
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 00:18:59 -0500

    feat: :sparkles: add validation rules and fields to CreateCorreoDto and UpdateCorreoDto

--- src/correos/dto/create-correo.dto.ts
+++ src/correos/dto/create-correo.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCorreoDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCorreoDto {
+  @IsNotEmpty({ message: 'El correo es requerido' })
+  @IsString({ message: 'El correo debe ser de tipo stirng' })
+  cuenta: string;
+
+  @IsNotEmpty()
+  @IsString({ message: 'La fecha de creacion debe ser de tipo date' })
+  clave_ingreso: string;
+}
