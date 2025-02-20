7dcb01aefd2b4b039db0a336f1e21cdf58692b42
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 00:51:02 -0500

    feat: :sparkles: add validation rules and fields to CreatePersonaRepresentanteDto and UpdatePersonaRepresentanteDto

--- src/persona_representante/dto/create-persona_representante.dto.ts
+++ src/persona_representante/dto/create-persona_representante.dto.ts
@@ -1,1 +1,19 @@
-export class CreatePersonaRepresentanteDto {}
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePersonaRepresentanteDto {
+  @IsNotEmpty({ message: 'El nit es requerido' })
+  @IsInt({ message: 'El nit debe ser de tipo entero' })
+  nit: number;
+
+  @IsNotEmpty({ message: 'El dv es requerido' })
+  @IsInt({ message: 'El dv debe ser de tipo entero' })
+  dv: number;
+
+  @IsNotEmpty({ message: 'El nombre de la empresa es requerido' })
+  @IsString({ message: 'El nombre de la empresa debe ser de tipo string' })
+  nombre_sas: string;
+
+  @IsNotEmpty({ message: 'El id de la persona representada es requerido' })
+  @IsInt({ message: 'El id debe ser de tipo entero' })
+  id_persona_representante: number;
+}
--- src/persona_representante/dto/update-persona_representante.dto.ts
+++ src/persona_representante/dto/update-persona_representante.dto.ts
@@ -1,6 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePersonaRepresentanteDto } from './create-persona_representante.dto';
+import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
 
-export class UpdatePersonaRepresentanteDto extends PartialType(CreatePersonaRepresentanteDto) {
+export class UpdatePersonaRepresentanteDto extends PartialType(
+  CreatePersonaRepresentanteDto,
+) {
+  @IsNotEmpty({ message: 'El id es requerido' })
+  @IsInt({ message: 'El id debe ser de tipo entero' })
   id: number;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de creacion debe ser de tipo date' })
+  created_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo date' })
+  updated_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo date' })
+  delted_at: Date;
 }
