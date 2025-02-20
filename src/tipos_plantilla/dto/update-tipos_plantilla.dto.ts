d57bf83cde6b41bc2402220f6601912e57aa0ecf
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 16:42:19 -0500

    fix: :card_file_box: remove unnecessary date fields from UpdateTiposPlantillaDto

--- src/tipos_plantilla/dto/update-tipos_plantilla.dto.ts
+++ src/tipos_plantilla/dto/update-tipos_plantilla.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTiposPlantillaDto } from './create-tipos_plantilla.dto';
-import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class UpdateTiposPlantillaDto extends PartialType(
   CreateTiposPlantillaDto,
@@ -8,16 +8,4 @@ export class UpdateTiposPlantillaDto extends PartialType(
   @IsNotEmpty({ message: 'El id es requerido' })
   @IsInt({ message: 'El id debe ser de tipo entero' })
   id: number;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de creacion debe ser de tipo date' })
-  created_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo date' })
-  updated_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo date' })
-  delted_at: Date;
 }
