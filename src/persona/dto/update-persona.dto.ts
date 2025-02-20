a2e826ccf4bd6c12939a8ab5b88e1e936c244e58
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 16:41:21 -0500

    fix: :card_file_box: remove unnecessary date fields from UpdatePersonaDto

--- src/persona/dto/update-persona.dto.ts
+++ src/persona/dto/update-persona.dto.ts
@@ -1,22 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePersonaDto } from './create-persona.dto';
-import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 // Usar este dto para realizar busquedas
 export class UpdatePersonaDto extends PartialType(CreatePersonaDto) {
   @IsNotEmpty({ message: 'El id no puede estar vacio' })
   @IsInt({ message: 'El id debe ser un número entero' })
   id: number;
-
-  @IsOptional()
-  @IsDate()
-  created_at: Date;
-
-  @IsOptional()
-  @IsDate()
-  updated_at: Date;
-
-  @IsOptional()
-  @IsDate()
-  deleted_at: Date;
 }
