d156411f304abb5996f5442f1005ad9f9791c86b
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 10:58:40 -0500

    feat(DB): :sparkles: add modulos table
    has been added modulos table

--- src/modulos/entities/modulo.entity.ts
+++ src/modulos/entities/modulo.entity.ts
@@ -1,1 +1,30 @@
-export class Modulo {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Modulo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    unique: true,
+    nullable: false,
+    comment: 'Nombre del modulo',
+  })
+  nombre: string;
+
+  @Column({
+    nullable: false,
+    comment: 'Fecha de creacion del modulo',
+  })
+  created_at: Date;
+
+  @Column({
+    nullable: false,
+    comment: 'Fecha de actualizacion del modulo',
+  })
+  updated_at: Date;
+
+  @Column({
+    comment: 'Fecha de eliminacion del modulo',
+  })
+  deleted_at: Date;
+}
