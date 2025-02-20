68f5f3ce70c841fe71cc9ba20113ff8c278dc937
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 16:46:09 -0500

    feat: :sparkles: add id and clave fields to Dian entity

--- src/dian/entities/dian.entity.ts
+++ src/dian/entities/dian.entity.ts
@@ -1,1 +1,17 @@
-export class Dian {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Dian {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    comment: 'Clave del usuario',
+  })
+  clave_ingreso: string;
+
+  @Column({
+    comment: 'Clave de la firma',
+  })
+  clave_firma: string;
+}
