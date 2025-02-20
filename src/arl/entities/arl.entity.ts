49194a7365c9dea366865576c2b87f5e63a6a917
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 17:54:48 -0500

    feat: :sparkles: add Arl entity with properties including id, usuario, clave_ingreso, and timestamp fields

--- src/arl/entities/arl.entity.ts
+++ src/arl/entities/arl.entity.ts
@@ -1,1 +1,33 @@
-export class Arl {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Arl {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    comment: 'Usuario de la ARL',
+  })
+  usuario: string;
+
+  @Column({
+    nullable: false,
+    comment: 'Clave de ingreso de la ARL',
+  })
+  clave_ingreso: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
