eeb639873d7c92f500583d7f7fad07ae21b68e6f
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 17:56:42 -0500

    feat: :sparkles: add Afp entity with properties including id, usuario, clave_ingreso, created_at, updated_at, and deleted_at fields

--- src/afp/entities/afp.entity.ts
+++ src/afp/entities/afp.entity.ts
@@ -1,1 +1,33 @@
-export class Afp {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Afp {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    comment: 'Usuario de la afp',
+  })
+  usuario: string;
+
+  @Column({
+    nullable: false,
+    comment: 'Clave de ingreso de la afp',
+  })
+  clave_ingreso: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
