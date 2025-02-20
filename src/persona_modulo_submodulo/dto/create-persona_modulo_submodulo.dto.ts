1d8652b20e733d363d08241eea51a0755fc6271c
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 00:46:15 -0500

    feat: :sparkles: add validation rules and fields to CreatePersonaModuloSubmoduloDto and UpdatePersonaModuloSubmoduloDto

--- src/persona_modulo_submodulo/dto/create-persona_modulo_submodulo.dto.ts
+++ src/persona_modulo_submodulo/dto/create-persona_modulo_submodulo.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePersonaModuloSubmoduloDto {}
+import { IsInt, IsNotEmpty } from 'class-validator';
+
+export class CreatePersonaModuloSubmoduloDto {
+  @IsNotEmpty({ message: 'El id_submodulo es requerido' })
+  @IsInt({ message: 'El id_submodulo debe ser de tipo entero' })
+  id_submodulo: number;
+
+  @IsNotEmpty({ message: 'El id_modulo es requerido' })
+  @IsInt({ message: 'El id_modulo debe ser de tipo entero' })
+  id_modulo: number;
+
+  @IsNotEmpty({ message: 'El id_persona es requerido' })
+  @IsInt({ message: 'El id_persona debe ser de tipo entero' })
+  id_persona: number;
+}
--- src/persona_modulo_submodulo/dto/update-persona_modulo_submodulo.dto.ts
+++ src/persona_modulo_submodulo/dto/update-persona_modulo_submodulo.dto.ts
@@ -1,6 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePersonaModuloSubmoduloDto } from './create-persona_modulo_submodulo.dto';
+import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
 
-export class UpdatePersonaModuloSubmoduloDto extends PartialType(CreatePersonaModuloSubmoduloDto) {
+export class UpdatePersonaModuloSubmoduloDto extends PartialType(
+  CreatePersonaModuloSubmoduloDto,
+) {
+  @IsNotEmpty({ message: 'El id es requerido' })
+  @IsInt({ message: 'El id debe ser de tipo entero' })
   id: number;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de creacion debe ser de tipo date' })
+  created_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo date' })
+  updated_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo date' })
+  delted_at: Date;
 }
