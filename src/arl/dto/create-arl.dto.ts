0db82e4cd5d7950c4f4279271dbe57ed1bdf343f
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-19 19:16:28 -0500

    feat: :sparkles: add validation rules and fields to CreateArlDto and UpdateArlDto

--- src/arl/dto/create-arl.dto.ts
+++ src/arl/dto/create-arl.dto.ts
@@ -1,1 +1,11 @@
-export class CreateArlDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateArlDto {
+  @IsNotEmpty({ message: 'El nombre de usuario es requerido' })
+  @IsString({ message: 'El nombre de usuario debe ser un texto' })
+  name: string;
+
+  @IsNotEmpty({ message: 'La clave de ingreso debe ser requerida' })
+  @IsString({ message: 'La clave de ingreso debe ser un texto' })
+  clave: string;
+}
--- src/arl/dto/update-arl.dto.ts
+++ src/arl/dto/update-arl.dto.ts
@@ -1,6 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateArlDto } from './create-arl.dto';
+import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateArlDto extends PartialType(CreateArlDto) {
+  @IsNotEmpty({ message: 'El id de la ARL es requerido' })
+  @IsInt({ message: 'El id de la ARL debe ser un número entero' })
   id: number;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de creación debe ser una fecha' })
+  created_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de actualización debe ser una fecha' })
+  updated_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de eliminación debe ser una fecha' })
+  deleted_at: Date;
 }
