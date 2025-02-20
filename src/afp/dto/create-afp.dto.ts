2fb5d2713b444414bb6e4956e0a0cd2411d35c4d
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-19 19:24:08 -0500

    feat: :sparkles: add validation rules and fields to CreateAfpDto and UpdateAfpDto

--- src/afp/dto/create-afp.dto.ts
+++ src/afp/dto/create-afp.dto.ts
@@ -1,1 +1,11 @@
-export class CreateAfpDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAfpDto {
+  @IsNotEmpty({ message: 'El usuario es requerido' })
+  @IsString({ message: 'El usuario debe ser un string' })
+  usuario: string;
+
+  @IsNotEmpty({ message: 'La clave de ingreso es requerida' })
+  @IsString({ message: 'La clave de ingreso debe ser un string' })
+  clave: string;
+}
