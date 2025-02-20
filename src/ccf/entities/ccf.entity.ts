afdf4e7e403ccce0b4eb6510ccda64c16c40891e
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 17:55:50 -0500

    feat: :sparkles: add Ccf entity with properties including id, usuario, clave_ingreso, created_at, updated_at, and deleted_at fields

--- src/ccf/entities/ccf.entity.ts
+++ src/ccf/entities/ccf.entity.ts
@@ -1,1 +1,33 @@
-export class Ccf {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Ccf {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    comment: 'Usuario de la Ccf',
+  })
+  usuario: string;
+
+  @Column({
+    nullable: false,
+    comment: 'Clave de ingreso de la Ccf',
+  })
+  clave_ingreso: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
