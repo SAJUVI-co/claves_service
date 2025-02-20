11832561b99ff44e96550415519ac83c92b8b04a
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 16:40:36 -0500

    fix: :card_file_box: remove unnecessary date fields from UpdateContadorPersonaDto

--- src/contador_persona/dto/update-contador_persona.dto.ts
+++ src/contador_persona/dto/update-contador_persona.dto.ts
@@ -1,12 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateContadorPersonaDto } from './create-contador_persona.dto';
-import {
-  IsDate,
-  IsInt,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class UpdateContadorPersonaDto extends PartialType(
   CreateContadorPersonaDto,
@@ -14,20 +8,4 @@ export class UpdateContadorPersonaDto extends PartialType(
   @IsNotEmpty({ message: 'El id de la persona no puede estar vacío' })
   @IsInt({ message: 'El id de la persona debe ser un número entero' })
   id: number;
-
-  @IsOptional()
-  @IsString({ message: 'La targeta profesional debe de ser un string' })
-  targeta_profesional: string;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de expedicion debe ser una fecha' })
-  created_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de actualizacion debe ser una fecha' })
-  updated_at: Date;
-
-  @IsOptional()
-  @IsDate({ message: 'La fecha de eliminacion debe ser una fecha' })
-  deleted_at: Date;
 }
