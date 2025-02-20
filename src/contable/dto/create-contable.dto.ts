bf8a397bb042f2b9ce19a0970f6761dc21e7e6ff
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 00:10:58 -0500

    feat: :sparkles: add validation rules and fields to CreateContableDto and UpdateContableDto

--- src/contable/dto/create-contable.dto.ts
+++ src/contable/dto/create-contable.dto.ts
@@ -1,1 +1,15 @@
-export class CreateContableDto {}
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateContableDto {
+  @IsNotEmpty({ message: 'El usuario es requerido' })
+  @IsString({ message: 'El usuario debe ser de tipo string' })
+  usuario: string;
+
+  @IsNotEmpty({ message: 'La clave de ingreso es requerida' })
+  @IsString({ message: 'La clave de ingreso debe ser de tipo string' })
+  clave_ingreso: string;
+
+  @IsNotEmpty({ message: 'El programa es requerido' })
+  @IsArray({ message: 'El programa debe ser de tipo string' })
+  programa: number[];
+}
--- src/contable/dto/update-contable.dto.ts
+++ src/contable/dto/update-contable.dto.ts
@@ -1,6 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateContableDto } from './create-contable.dto';
+import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateContableDto extends PartialType(CreateContableDto) {
+  @IsNotEmpty({ message: 'El id es requerido' })
+  @IsInt({ message: 'El id debe ser de tipo entero' })
   id: number;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de creacion debe ser de tipo date' })
+  created_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo date' })
+  updated_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo date' })
+  delted_at: Date;
 }
