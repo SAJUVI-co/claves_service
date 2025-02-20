fc39ac910f0269ce467762d61792f126a1aa0b6e
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 00:04:34 -0500

    feat: :sparkles: add validation rules and fields to CreateCamaraComercioDto and UpdateCamaraComercioDto

--- src/camara_comercio/dto/create-camara_comercio.dto.ts
+++ src/camara_comercio/dto/create-camara_comercio.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCamaraComercioDto {}
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCamaraComercioDto {
+  @IsNotEmpty({ message: 'La sucursal es requerida' })
+  @IsInt({ message: 'La sucursal debe ser de tipo entero' })
+  sucusarl: number;
+
+  @IsNotEmpty({ message: 'La pagina web es requerida' })
+  @IsString({ message: 'La pagina web debe ser de tipo string' })
+  pagina_web: string;
+
+  @IsNotEmpty({ message: 'El usuario es requerido' })
+  @IsString({ message: 'El usuario debe ser de tipo string' })
+  usuario: string;
+
+  @IsNotEmpty({ message: 'La clave de ingreso es requerida' })
+  @IsString({ message: 'La clave de ingreso debe ser de tipo string' })
+  clave_ingreso: string;
+}
--- src/camara_comercio/dto/update-camara_comercio.dto.ts
+++ src/camara_comercio/dto/update-camara_comercio.dto.ts
@@ -1,6 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCamaraComercioDto } from './create-camara_comercio.dto';
+import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
 
-export class UpdateCamaraComercioDto extends PartialType(CreateCamaraComercioDto) {
+export class UpdateCamaraComercioDto extends PartialType(
+  CreateCamaraComercioDto,
+) {
+  @IsNotEmpty({ message: 'El id es requerido' })
+  @IsInt({ message: 'El id debe ser de tipo entero' })
   id: number;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de creacion debe ser de tipo date' })
+  created_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo date' })
+  updated_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo date' })
+  delted_at: Date;
 }
