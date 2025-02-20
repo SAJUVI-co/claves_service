614e4b4d0df0456db73b325e6745c88d3f6cd0ee
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-19 23:58:01 -0500

    feat: :sparkles: add validation rules and fields to CreateAlcaldiaDto and UpdateAlcaldiaDto

--- src/alcaldia/dto/create-alcaldia.dto.ts
+++ src/alcaldia/dto/create-alcaldia.dto.ts
@@ -1,1 +1,31 @@
-export class CreateAlcaldiaDto {}
+import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateAlcaldiaDto {
+  @IsNotEmpty({ message: 'El campo cuidad no puede estar vacio' })
+  @IsInt({ message: 'El campo cuidad debe ser un numero entero' })
+  cuidad: number;
+
+  @IsNotEmpty({ message: 'La pagina web es requerida' })
+  @IsString({ message: 'La pagina web debe ser de tipo string' })
+  paginaWeb: string;
+
+  @IsNotEmpty({ message: 'La identificacion es requerida' })
+  @IsString({ message: 'La idenficacion debe ser de tipo string' })
+  identificacion: string;
+
+  @IsNotEmpty({ message: 'La clave de ingreso es requerida' })
+  @IsString({ message: 'La clave de ingreso debe ser de tipo string' })
+  clave_ingreso: string;
+
+  @IsOptional()
+  @IsString({ message: 'La clave exogena debe ser de tipo string' })
+  clave_exogena: string;
+
+  @IsOptional()
+  @IsString({ message: 'La clave ICA debe ser de tipo string' })
+  clave_ica: string;
+
+  @IsNotEmpty({ message: 'La placa es requerida' })
+  @IsString({ message: 'La placa debe ser de tipo interger' })
+  placa: number;
+}
--- src/alcaldia/dto/update-alcaldia.dto.ts
+++ src/alcaldia/dto/update-alcaldia.dto.ts
@@ -1,6 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAlcaldiaDto } from './create-alcaldia.dto';
+import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateAlcaldiaDto extends PartialType(CreateAlcaldiaDto) {
+  @IsNotEmpty({ message: 'El campo id no puede estar vacio' })
+  @IsInt({ message: 'El campo id debe ser un numero entero' })
   id: number;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de creacion debe ser de tipo date' })
+  created_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo date' })
+  updated_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo date' })
+  delted_at: Date;
 }
