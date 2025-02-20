06f4e341d90768d5cf83d0e9b2ed1ddfe0838155
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 17:43:10 -0500

    feat: :sparkles: add Eps entity with properties including id, usuario, clave_ingreso, and timestamp fields

--- src/eps/entities/ep.entity.ts
+++ src/eps/entities/ep.entity.ts
@@ -1,1 +1,33 @@
-export class Ep {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Eps {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    comment: 'Nombre de la EPS',
+  })
+  usuario: string;
+
+  @Column({
+    nullable: false,
+    comment: 'Clave de la EPS',
+  })
+  clave_ingreso: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
