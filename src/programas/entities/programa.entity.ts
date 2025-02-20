f3d8b31cbdc4140b939211819901f02d1ffa8fda
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 11:48:22 -0500

    feat: :sparkles: define Programa entity with fields and validation rules

--- src/programas/entities/programa.entity.ts
+++ src/programas/entities/programa.entity.ts
@@ -1,1 +1,35 @@
-export class Programa {}
+import { Column, PrimaryColumn } from 'typeorm';
+
+export class Programa {
+  @PrimaryColumn()
+  id: number;
+
+  @Column({
+    unique: true,
+    nullable: false,
+  })
+  nombre: string;
+
+  @Column({
+    unique: true,
+    nullable: false,
+  })
+  pagina_web: string;
+
+  @Column({
+    nullable: false,
+    comment: 'Fecha de creacion del modulo',
+  })
+  created_at: Date;
+
+  @Column({
+    nullable: false,
+    comment: 'Fecha de actualizacion del modulo',
+  })
+  updated_at: Date;
+
+  @Column({
+    comment: 'Fecha de eliminacion del modulo',
+  })
+  deleted_at: Date;
+}
