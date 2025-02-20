7b32173bd8f34f0343b31d4cb47e94053f913fbd
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-19 19:03:15 -0500

    feat: :sparkles: add validation rules and fields to CreatePlanillaSsDto and UpdatePlanillaSsDto

--- src/planilla_ss/dto/create-planilla_ss.dto.ts
+++ src/planilla_ss/dto/create-planilla_ss.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePlanillaSsDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePlanillaSsDto {
+  @IsNotEmpty({
+    message: 'El nombre de usuario de la seguridad social, no debe estar vacio',
+  })
+  @IsString({
+    message: 'El nombre de usuario de la seguridad social, debe ser un texto',
+  })
+  nombre_usuario: string;
+
+  @IsNotEmpty({ message: 'La clave de ingreso no puede estar vacia' })
+  @IsString({ message: 'La clave de ingreso debe ser un texto' })
+  clave_ingreso: string;
+}
--- src/planilla_ss/dto/update-planilla_ss.dto.ts
+++ src/planilla_ss/dto/update-planilla_ss.dto.ts
@@ -1,6 +1,26 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePlanillaSsDto } from './create-planilla_ss.dto';
+import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdatePlanillaSsDto extends PartialType(CreatePlanillaSsDto) {
+  @IsNotEmpty({
+    message: 'El id de la planilla de seguridad social no puede estar vacio',
+  })
+  @IsInt({
+    message:
+      'El id de la planilla de seguridad social debe ser un numero entero',
+  })
   id: number;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de creacion debe ser una fecha valida' })
+  created_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de actualizacion debe ser una fecha valida' })
+  udpated_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de eliminacion debe ser una fecha valida' })
+  deleted_at: Date;
 }
