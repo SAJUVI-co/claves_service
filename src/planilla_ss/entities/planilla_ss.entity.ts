6a527c74febe4b3c56cf6e2c2a1a19ef886af032
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 17:19:21 -0500

    feat: :sparkles: add PlanillaSs entity with properties including id, usuario, clave_ingreso, and timestamp fields

--- src/planilla_ss/entities/planilla_ss.entity.ts
+++ src/planilla_ss/entities/planilla_ss.entity.ts
@@ -1,1 +1,33 @@
-export class PlanillaSs {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class PlanillaSs {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    comment: 'Usuario de la planilla',
+  })
+  usuario: string;
+
+  @Column({
+    nullable: false,
+    comment: 'Fecha de la planilla',
+  })
+  clave_ingreso: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
