40af5426c0a6e61cf4e1801091338b62eb70dde8
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 16:40:09 -0500

    fix: :card_file_box: remove unnecessary date fields from UpdateAfpDto

--- src/afp/dto/update-afp.dto.ts
+++ src/afp/dto/update-afp.dto.ts
@@ -1,21 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAfpDto } from './create-afp.dto';
-import { IsDate, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateAfpDto extends PartialType(CreateAfpDto) {
   @IsNotEmpty({ message: 'El id de la CCF es requerido' })
   @IsString({ message: 'El id de la CCF debe ser un número entero' })
   id: number;
-
-  @IsOptional({ message: 'La fecha de creación debe ser una fecha' })
-  @IsDate()
-  created_at: Date;
-
-  @IsOptional({ message: 'La fecha de actualización debe ser una fecha' })
-  @IsDate()
-  updated_at: Date;
-
-  @IsOptional({ message: 'La fecha de eliminación debe ser una fecha' })
-  @IsDate()
-  deleted_at: Date;
 }
