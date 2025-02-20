d26277883392839e9cc9be7e93f56372584a8f6b
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 01:12:43 -0500

    feat: :sparkles: add validation rules and fields to CreateTiposPlantillaDto and UpdateTiposPlantillaDto

--- src/tipos_plantilla/dto/create-tipos_plantilla.dto.ts
+++ src/tipos_plantilla/dto/create-tipos_plantilla.dto.ts
@@ -1,1 +1,11 @@
-export class CreateTiposPlantillaDto {}
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTiposPlantillaDto {
+  @IsNotEmpty({ message: 'El nombre es requerido' })
+  @IsString({ message: 'El nombre debe ser de tipo string' })
+  nombre: string;
+
+  @IsNotEmpty({ message: 'El submodulo_id es requerido' })
+  @IsInt({ message: 'El submodulo_id debe ser de tipo entero' })
+  submodulo_id: number;
+}
--- src/tipos_plantilla/dto/update-tipos_plantilla.dto.ts
+++ src/tipos_plantilla/dto/update-tipos_plantilla.dto.ts
@@ -1,6 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTiposPlantillaDto } from './create-tipos_plantilla.dto';
+import { IsDate, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
 
-export class UpdateTiposPlantillaDto extends PartialType(CreateTiposPlantillaDto) {
+export class UpdateTiposPlantillaDto extends PartialType(
+  CreateTiposPlantillaDto,
+) {
+  @IsNotEmpty({ message: 'El id es requerido' })
+  @IsInt({ message: 'El id debe ser de tipo entero' })
   id: number;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de creacion debe ser de tipo date' })
+  created_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de actualizacion debe ser de tipo date' })
+  updated_at: Date;
+
+  @IsOptional()
+  @IsDate({ message: 'La fecha de eliminacion debe ser de tipo date' })
+  delted_at: Date;
 }
