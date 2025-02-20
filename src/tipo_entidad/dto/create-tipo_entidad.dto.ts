9e2a1fc13d2c3dbd0ffc91bff74697f804296a4e
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 16:42:15 -0500

    feat: :sparkles: add validation to CreateTipoEntidadDto fields

--- src/tipo_entidad/dto/create-tipo_entidad.dto.ts
+++ src/tipo_entidad/dto/create-tipo_entidad.dto.ts
@@ -1,1 +1,14 @@
-export class CreateTipoEntidadDto {}
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTipoEntidadDto {
+  @IsNotEmpty({ message: 'El nombre es requerido' })
+  nombre: string;
+
+  @IsNotEmpty({ message: 'La pagina web es requerida' })
+  @IsString({ message: 'La pagina web debe ser un string' })
+  pagina_web: string;
+
+  @IsNotEmpty({ message: 'El id del submodulo es requerido' })
+  @IsInt({ message: 'El id del submodulo debe ser un entero' })
+  submodulo_id: number;
+}
