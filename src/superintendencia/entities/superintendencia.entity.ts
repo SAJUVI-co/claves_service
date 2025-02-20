700a5d3cf8194035c6f2979b8fa9675bbbae9660
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 17:51:26 -0500

    feat: :sparkles: add Superintendencia entity with properties including id, usuario, clave_ingreso, and timestamp fields

--- src/superintendencia/entities/superintendencia.entity.ts
+++ src/superintendencia/entities/superintendencia.entity.ts
@@ -1,1 +1,33 @@
-export class Superintendencia {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Superintendencia {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    comment: 'Usuario de la superintendencia',
+  })
+  usuario: string;
+
+  @Column({
+    nullable: false,
+    comment: 'Clave de ingreso de la superintendencia',
+  })
+  clave_ingreso: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
