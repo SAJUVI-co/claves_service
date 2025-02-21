4b9422c605255025e125a8493d73cffbd952b88b
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-21 11:14:25 -0500

    fix: :bug: rename 'nombre' field to 'cuidad' in CreateCuidadDto

--- src/cuidad/dto/create-cuidad.dto.ts
+++ src/cuidad/dto/create-cuidad.dto.ts
@@ -3,5 +3,5 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateCuidadDto {
   @IsNotEmpty({ message: 'El nombre de la cuidad es requerido' })
   @IsString({ message: 'El nombre de la cuidad debe ser un texto' })
-  nombre: string;
+  cuidad: string;
 }
