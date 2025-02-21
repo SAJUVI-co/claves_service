b5c69cce126555f0f23cbef14d03060cfe1281ee
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-21 11:44:29 -0500

    feat: :sparkles: add validation rules for 'id' field in UpdateTipoEntidadDto

--- src/tipo_entidad/dto/update-tipo_entidad.dto.ts
+++ src/tipo_entidad/dto/update-tipo_entidad.dto.ts
@@ -1,6 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTipoEntidadDto } from './create-tipo_entidad.dto';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class UpdateTipoEntidadDto extends PartialType(CreateTipoEntidadDto) {
+  @IsNotEmpty({ message: 'El id es requerido' })
+  @IsInt({ message: 'El id debe ser un número entero' })
   id: number;
 }
