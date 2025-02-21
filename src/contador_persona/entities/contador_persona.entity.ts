4811f0454d3aeda9d7d568ae30c6a8a10b413d4f
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 22:17:18 -0500

    feat: :sparkles: extend ContadorPersona class to inherit from DatesBase and add non-nullable fields with comments

--- src/contador_persona/entities/contador_persona.entity.ts
+++ src/contador_persona/entities/contador_persona.entity.ts
@@ -1,1 +1,31 @@
-export class ContadorPersona {}
+import { Persona } from 'src/persona/entities/persona.entity';
+import { DatesBase } from 'src/utils/base_dates.entity';
+import { Column, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+export class ContadorPersona extends DatesBase {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @OneToOne(() => Persona, (p) => p.id)
+  @JoinColumn()
+  @Column({
+    nullable: false,
+    comment: 'Relacion persona',
+  })
+  id_contador: number;
+
+  @OneToOne(() => Persona, (p) => p.id)
+  @JoinColumn()
+  @Column({
+    nullable: false,
+    comment: 'Relacion persona',
+  })
+  @JoinColumn()
+  id_persona: number;
+
+  @Column({
+    nullable: false,
+    comment: 'Relacion persona',
+  })
+  targeta_profesional: string;
+}
