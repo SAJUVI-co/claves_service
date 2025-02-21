5e928735e36cc9db993399525c3831993c702d58
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 22:12:51 -0500

    feat: :sparkles: add non-nullable id_programa and id_contable fields to ContablePrograma entity with comments

--- src/contable_programa/entities/contable_programa.entity.ts
+++ src/contable_programa/entities/contable_programa.entity.ts
@@ -1,12 +1,20 @@
 import { Programa } from 'src/programas/entities/programa.entity';
 import { DatesBase } from 'src/utils/base_dates.entity';
-import { JoinColumn, OneToMany } from 'typeorm';
+import { Column, JoinColumn, OneToMany } from 'typeorm';
 
 export class ContablePrograma extends DatesBase {
   @OneToMany(() => Programa, (p) => p.id)
   @JoinColumn()
+  @Column({
+    nullable: false,
+    comment: 'relacion del programa',
+  })
   id_programa: number;
 
   @JoinColumn()
+  @Column({
+    nullable: false,
+    comment: 'relacion tabla contable',
+  })
   id_contable: number;
 }
