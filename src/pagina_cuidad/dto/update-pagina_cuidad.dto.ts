3556624c811b7ccc2d729bb87c1eaeee84cd271f
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-21 11:29:45 -0500

    feat: :sparkles: add validation rules for 'id' field in UpdatePaginaCuidadDto

--- src/pagina_cuidad/dto/update-pagina_cuidad.dto.ts
+++ src/pagina_cuidad/dto/update-pagina_cuidad.dto.ts
@@ -1,6 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePaginaCuidadDto } from './create-pagina_cuidad.dto';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class UpdatePaginaCuidadDto extends PartialType(CreatePaginaCuidadDto) {
+  @IsNotEmpty({ message: 'El id es requerido' })
+  @IsInt({ message: 'El id debe ser de tipo numero' })
   id: number;
 }
