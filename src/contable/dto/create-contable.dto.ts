b80ccf490dcf2811df84e921758260bf2337899d
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 19:42:13 -0500

    fix: :fire: deleted unnecesary code on create-contable.dto.ts
    has been deleted the las fields added becouse are bad integrated
    
    Commit: 65cfb2b

--- src/contable/dto/create-contable.dto.ts
+++ src/contable/dto/create-contable.dto.ts
@@ -1,4 +1,4 @@
-import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateContableDto {
   @IsNotEmpty({ message: 'El usuario es requerido' })
@@ -8,12 +8,4 @@ export class CreateContableDto {
   @IsNotEmpty({ message: 'La clave de ingreso es requerida' })
   @IsString({ message: 'La clave de ingreso debe ser de tipo string' })
   clave_ingreso: string;
-
-  @IsNotEmpty({ message: 'El programa es requerido' })
-  @IsInt({ message: 'El programa debe ser de tipo enterno' })
-  id_programa: number;
-
-  @IsNotEmpty({ message: 'El programa es requerido' })
-  @IsInt({ message: 'El programa debe ser de tipo number' })
-  id_contable: number;
 }
