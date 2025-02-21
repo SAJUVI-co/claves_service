83d353c78f2840703775704d8fedf683091442fe
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-21 09:43:11 -0500

    feat: :sparkles: extend CamaraComercio class from SubModuleBase; add OneToMany relationship with PaginaCuidad and define sucursal column

--- src/camara_comercio/entities/camara_comercio.entity.ts
+++ src/camara_comercio/entities/camara_comercio.entity.ts
@@ -1,1 +1,14 @@
-export class CamaraComercio {}
+import { PaginaCuidad } from 'src/pagina_cuidad/entities/pagina_cuidad.entity';
+import { SubModuleBase } from 'src/utils/submodules_base';
+import { Column, Entity, JoinColumn, OneToMany } from 'typeorm';
+
+@Entity()
+export class CamaraComercio extends SubModuleBase {
+  @OneToMany(() => PaginaCuidad, (pa) => pa.id)
+  @JoinColumn()
+  @Column({
+    nullable: false,
+    comment: 'sucursal',
+  })
+  sucursal: number;
+}
