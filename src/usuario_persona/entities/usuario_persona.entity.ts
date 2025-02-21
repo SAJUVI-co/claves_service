a960ba978f5b8a7a93b150d2d977f979b8ca44b8
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 22:32:16 -0500

    ``` feat: :sparkles: implement new validation logic for user input fields ```

--- src/usuario_persona/entities/usuario_persona.entity.ts
+++ src/usuario_persona/entities/usuario_persona.entity.ts
@@ -1,1 +1,30 @@
-export class UsuarioPersona {}
+import { Persona } from 'src/persona/entities/persona.entity';
+import { DatesBase } from 'src/utils/base_dates.entity';
+import {
+  Column,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+export class UsuarioPersona extends DatesBase {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @OneToMany(() => Persona, (p) => p.id)
+  @JoinColumn()
+  @Column({
+    nullable: false,
+    comment: 'relacion persona',
+  })
+  id_persona: number;
+
+  @OneToOne(() => Persona, (p) => p.id)
+  @JoinColumn()
+  @Column({
+    nullable: false,
+    comment: 'relacion persona',
+  })
+  id_user: number;
+}
