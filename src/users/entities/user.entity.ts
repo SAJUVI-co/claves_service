d013d6551babfb648e62dc5221604370e35caa16
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 22:06:47 -0500

    feat: :sparkles: extend User class to inherit from DatesBase and add id and user_id fields

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { DatesBase } from 'src/utils/base_dates.entity';
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class User extends DatesBase {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: true,
+    comment: 'Id del usuario almacenado en el servicio',
+  })
+  user_id: number;
+}
