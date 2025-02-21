693d06af1e06ef0d671543bd522a6bc3860d9dc6
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-21 11:29:42 -0500

    feat: :sparkles: add validation rules to CreatePaginaCuidadDto for cuidad_id and pagina fields

--- src/pagina_cuidad/dto/create-pagina_cuidad.dto.ts
+++ src/pagina_cuidad/dto/create-pagina_cuidad.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePaginaCuidadDto {}
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePaginaCuidadDto {
+  @IsNotEmpty({ message: 'La cuidad es requerida' })
+  @IsInt({ message: 'La cuidad debe ser un numero' })
+  cuidad_id: number;
+
+  @IsNotEmpty({ message: 'La pagina es requerida' })
+  @IsString({ message: 'La pagina debe ser un texto' })
+  pagina: string;
+}
