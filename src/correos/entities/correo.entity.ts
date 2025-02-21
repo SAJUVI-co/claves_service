9bc8c1c981d198099bbd079c3ff97a509225bd69
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-21 09:59:04 -0500

    feat: :sparkles: enhance Correo entity with id, email, and password fields; extend from DatesBase

--- src/correos/entities/correo.entity.ts
+++ src/correos/entities/correo.entity.ts
@@ -1,1 +1,19 @@
-export class Correo {}
+import { DatesBase } from 'src/utils/base_dates.entity';
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Correo extends DatesBase {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    comment: 'correo',
+  })
+  email: string;
+
+  @Column({
+    nullable: false,
+    comment: 'password',
+  })
+  password: string;
+}
