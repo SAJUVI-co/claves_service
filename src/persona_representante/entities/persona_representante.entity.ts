9faee33a0c7c50f6fdea1238734421e5fbc50e0a
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 23:05:05 -0500

    feat: :sparkles: implement PersonaRepresentante class extending DatesBase with non-nullable fields and OneToOne relationships to Persona

--- src/persona_representante/entities/persona_representante.entity.ts
+++ src/persona_representante/entities/persona_representante.entity.ts
@@ -1,1 +1,45 @@
-export class PersonaRepresentante {}
+import { Persona } from 'src/persona/entities/persona.entity';
+import { DatesBase } from 'src/utils/base_dates.entity';
+import { Column, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+export class PersonaRepresentante extends DatesBase {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    unique: true,
+    comment: 'dv',
+  })
+  nit: number;
+
+  @Column({
+    nullable: false,
+    comment: 'dv',
+  })
+  dv: number;
+
+  @Column({
+    nullable: false,
+    unique: true,
+    comment: 'dv',
+  })
+  nombre_sas: string;
+
+  @OneToOne(() => Persona, (p) => p.id)
+  @JoinColumn()
+  @Column({
+    nullable: false,
+    comment: 'relacion del representante',
+  })
+  id_persona_representante: number;
+
+  @OneToOne(() => Persona, (p) => p.id)
+  @JoinColumn()
+  @Column({
+    nullable: false,
+    unique: true,
+    comment: 'relacion de la persona juridica',
+  })
+  id_persona: number;
+}
