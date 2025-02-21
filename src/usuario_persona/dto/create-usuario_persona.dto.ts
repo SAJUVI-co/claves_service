9395fc4d4aa9db920a68c4b431de66557388ac68
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-21 11:54:43 -0500

    feat: :sparkles: add validation rules for 'id_persona' and 'id_user' fields in CreateUsuarioPersonaDto

--- src/usuario_persona/dto/create-usuario_persona.dto.ts
+++ src/usuario_persona/dto/create-usuario_persona.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUsuarioPersonaDto {}
+import { IsInt, IsNotEmpty } from 'class-validator';
+
+export class CreateUsuarioPersonaDto {
+  @IsInt({ message: 'El id de la persona debe ser de tipo entero' })
+  id_persona: number;
+
+  @IsNotEmpty({ message: 'El id de la persona es requerido' })
+  @IsInt({ message: 'El id de la persona debe ser de tipo entero' })
+  id_user: number;
+}
