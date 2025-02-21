55cd4b5046f2c72e8bad0015f82a109c846ac184
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-21 11:06:48 -0500

    refactor: :recycle: ormat UpdateContableProgramaDto for improved readability

--- src/contable_programa/dto/update-contable_programa.dto.ts
+++ src/contable_programa/dto/update-contable_programa.dto.ts
@@ -1,6 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateContableProgramaDto } from './create-contable_programa.dto';
 
-export class UpdateContableProgramaDto extends PartialType(CreateContableProgramaDto) {
+export class UpdateContableProgramaDto extends PartialType(
+  CreateContableProgramaDto,
+) {
   id: number;
 }
