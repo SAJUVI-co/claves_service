41174f60c79d549a66fdf6f5b64311843b202db7
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-21 10:21:12 -0500

    feat: :sparkles: create TipoEntidad class extending DatesBase; add id, nombre, pagina_web, and submodulo_id fields

--- src/tipo_entidad/entities/tipo_entidad.entity.ts
+++ src/tipo_entidad/entities/tipo_entidad.entity.ts
@@ -1,1 +1,26 @@
-export class TipoEntidad {}
+import { DatesBase } from 'src/utils/base_dates.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class TipoEntidad extends DatesBase {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    comment: 'nombre del tipo de entidad',
+  })
+  nombre: string;
+
+  @Column({
+    nullable: false,
+    comment: 'pagina web de la entidad',
+  })
+  pagina_web: string;
+
+  @Column({
+    nullable: false,
+    comment: 'id del submodulo que lo usa',
+  })
+  submodulo_id: number;
+}
