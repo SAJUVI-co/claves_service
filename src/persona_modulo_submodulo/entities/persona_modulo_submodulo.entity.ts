fffed8a111eebad583d2655820d4fd51634b4152
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 22:44:19 -0500

    feat: :sparkles: extend PersonaModuloSubmodulo class to inherit from DatesBase and add non-nullable fields with comments

--- src/persona_modulo_submodulo/entities/persona_modulo_submodulo.entity.ts
+++ src/persona_modulo_submodulo/entities/persona_modulo_submodulo.entity.ts
@@ -1,1 +1,28 @@
-export class PersonaModuloSubmodulo {}
+import { Modulo } from 'src/modulos/entities/modulo.entity';
+import { DatesBase } from 'src/utils/base_dates.entity';
+import { Column, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+export class PersonaModuloSubmodulo extends DatesBase {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    comment: 'relacion de los submodulos',
+  })
+  id_submodulo: number;
+
+  @OneToOne(() => Modulo, (m) => m.id)
+  @JoinColumn()
+  @Column({
+    nullable: false,
+    comment: 'relacion de los modulos',
+  })
+  id_modulo: number;
+
+  @Column({
+    nullable: false,
+    comment: 'relacion de las personas',
+  })
+  id_persona: number;
+}
