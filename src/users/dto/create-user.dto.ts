598a1bbf14b82bb233908484c363a1cbc90f4b60
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-21 11:51:06 -0500

    feat: :fire: remove unused 'personas' field from CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,7 @@
-import { IsArray, IsInt, IsNotEmpty } from 'class-validator';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty({ message: 'El user_id es requerido' })
   @IsInt({ message: 'El user_id debe ser de tipo entero' })
   user_id: number;
-
-  @IsNotEmpty({ message: 'Persona es requerido' })
-  @IsArray({ message: 'El personas debe ser de tipo entero' })
-  personas: number[];
 }
