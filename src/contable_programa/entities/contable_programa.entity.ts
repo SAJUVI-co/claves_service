c33aee3a9ec3e19f3053e3de03ff054553155a79
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 22:03:49 -0500

    feat: :sparkles: extend ContablePrograma class to inherit from DatesBase and add relationships with Programa

--- src/contable_programa/entities/contable_programa.entity.ts
+++ src/contable_programa/entities/contable_programa.entity.ts
@@ -1,1 +1,12 @@
-export class ContablePrograma {}
+import { Programa } from 'src/programas/entities/programa.entity';
+import { DatesBase } from 'src/utils/base_dates.entity';
+import { JoinColumn, OneToMany } from 'typeorm';
+
+export class ContablePrograma extends DatesBase {
+  @OneToMany(() => Programa, (p) => p.id)
+  @JoinColumn()
+  id_programa: number;
+
+  @JoinColumn()
+  id_contable: number;
+}
