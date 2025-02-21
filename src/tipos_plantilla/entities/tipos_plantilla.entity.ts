7ca7612ee75e5dc18ed91edf6e72b30ad8976c5a
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-21 10:23:37 -0500

    feat: :sparkles: update TiposPlantilla entity to change relationship from ManyToMany to ManyToOne with PlanillaSs

--- src/tipos_plantilla/entities/tipos_plantilla.entity.ts
+++ src/tipos_plantilla/entities/tipos_plantilla.entity.ts
@@ -1,12 +1,12 @@
 import { PlanillaSs } from 'src/planilla_ss/entities/planilla_ss.entity';
 import { DatesBase } from 'src/utils/base_dates.entity';
-import { Column, JoinColumn, ManyToMany } from 'typeorm';
+import { Column, JoinColumn, ManyToOne } from 'typeorm';
 
 export class TiposPlantilla extends DatesBase {
   @Column()
   nombre: string;
 
-  @ManyToMany(() => PlanillaSs, (planillaSs) => planillaSs.id)
+  @ManyToOne(() => PlanillaSs, (planillaSs) => planillaSs.id)
   @JoinColumn()
   submodulo_id: number;
 }
