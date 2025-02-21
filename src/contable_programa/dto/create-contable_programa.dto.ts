d690019e1e02a3224ed8038360657e04faf01555
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 19:44:44 -0500

    feat: :sparkles: add validation to CreateContableProgramaDto with id_programa and id_contable fields

--- src/contable_programa/dto/create-contable_programa.dto.ts
+++ src/contable_programa/dto/create-contable_programa.dto.ts
@@ -1,1 +1,11 @@
-export class CreateContableProgramaDto {}
+import { IsInt, IsNotEmpty } from 'class-validator';
+
+export class CreateContableProgramaDto {
+  @IsNotEmpty({ message: 'El programa no puede estar vacio' })
+  @IsInt({ message: 'El programa debe ser de tipo number' })
+  id_programa: number;
+
+  @IsNotEmpty({ message: 'El contable no puede estar vacio' })
+  @IsInt({ message: 'El contable debe ser de tipo number' })
+  id_contable: number;
+}
