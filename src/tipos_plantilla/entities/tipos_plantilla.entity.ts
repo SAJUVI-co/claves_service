d0494b7bc9c5ca9de8fb85da98c80fdc410d8d9b
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 21:47:15 -0500

    feat: :sparkles: extend TiposPlantilla class to inherit from DatesBase and add nombre and submodulo_id fields

--- src/tipos_plantilla/entities/tipos_plantilla.entity.ts
+++ src/tipos_plantilla/entities/tipos_plantilla.entity.ts
@@ -1,1 +1,12 @@
-export class TiposPlantilla {}
+import { PlanillaSs } from 'src/planilla_ss/entities/planilla_ss.entity';
+import { DatesBase } from 'src/utils/base_dates.entity';
+import { Column, JoinColumn, ManyToMany } from 'typeorm';
+
+export class TiposPlantilla extends DatesBase {
+  @Column()
+  nombre: string;
+
+  @ManyToMany(() => PlanillaSs, (planillaSs) => planillaSs.id)
+  @JoinColumn()
+  submodulo_id: number;
+}
