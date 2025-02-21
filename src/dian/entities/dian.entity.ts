2055d96cd26d86086f247a54b7685e8ad2929758
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-20 20:55:42 -0500

    feat: :sparkles: refactor Dian entity to extend SubModuleBase and remove unnecessary fields

--- src/dian/entities/dian.entity.ts
+++ src/dian/entities/dian.entity.ts
@@ -1,32 +1,9 @@
-import {
-  Column,
-  CreateDateColumn,
-  DeleteDateColumn,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { SubModuleBase } from 'src/utils/base_table.entity';
+import { Column } from 'typeorm';
 
-export class Dian {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column({
-    nullable: false,
-    comment: 'Clave del usuario',
-  })
-  clave_ingreso: string;
-
+export class Dian extends SubModuleBase {
   @Column({
     comment: 'Clave de la firma',
   })
   clave_firma: string;
-
-  @CreateDateColumn()
-  created_at: Date;
-
-  @UpdateDateColumn()
-  updated_at: Date;
-
-  @DeleteDateColumn()
-  deleted_at: Date;
 }
