c74293162d7944059eb11bf9c7753e0b7016aa50
CODE-G-ROOT <juanavila8856@gmail.com>
2025-02-21 21:38:37 -0500

    feat: :fire: UpdateUserDto as it is no longer needed

--- src/users/dto/update-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
-import { IsInt, IsNotEmpty } from 'class-validator';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsNotEmpty({ message: 'El id es requerido' })
-  @IsInt({ message: 'El id debe ser de tipo entero' })
-  id: number;
-}
